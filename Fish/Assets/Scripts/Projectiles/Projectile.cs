971726190ac258901bd3aee34b8d5a2933756881
owanray11 <petersr2919@my.uwstout.edu>
2023-02-11 16:28:10 -0600

    Made projectiles destroy

--- Fish/Assets/Scripts/Projectiles/Projectile.cs
+++ Fish/Assets/Scripts/Projectiles/Projectile.cs
@@ -6,9 +6,21 @@ public class Projectile : MonoBehaviour
 {
 
     [SerializeField] private float moveSpeed;
+    [SerializeField] private float lifeTime;
+
+    private void Start()
+    {
+        StartCoroutine(ProjectileDeath());
+    }
 
     private void Update()
     {
         gameObject.transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+    }
+
+    private IEnumerator ProjectileDeath()
+    {
+        yield return new WaitForSeconds(lifeTime);
+        Destroy(gameObject);
     }
 }
--- Fish/Assets/Scripts/Projectiles/ProjectileShooter.cs
+++ Fish/Assets/Scripts/Projectiles/ProjectileShooter.cs
@@ -11,15 +11,40 @@ public class ProjectileShooter : MonoBehaviour
     [SerializeField] private GameObject projectile;
     [SerializeField] float projectileSpeed = 1;
 
+    [Header("For debug")]
+    [SerializeField] private bool fireProjectiles;
+    [SerializeField] private float shotDelay = 1.0f;
+    private bool isCoroutineRunning;
+
     private Vector3 displacementVec;
     private float displacementAngle;
 
     private void Start()
     {
+        fireProjectiles = false;
         player = FindObjectOfType<PlayerInput>().gameObject;
         playerTransform = player.GetComponent<Transform>();
-        FireProjectile();
+        StartCoroutine(TestFire());
     }
+
+    /*private void Update()
+    {
+        if(fireProjectiles)
+        {
+            if (!isCoroutineRunning)
+            {
+                StartCoroutine(TestFire());
+                isCoroutineRunning = true;
+            }
+        } else if(!fireProjectiles)
+        {
+            if (isCoroutineRunning)
+            {
+                StopCoroutine(TestFire());
+                isCoroutineRunning = false;
+            }
+        }
+    }*/
 
     private void FireProjectile()
     {
@@ -28,5 +53,21 @@ public class ProjectileShooter : MonoBehaviour
         Debug.Log(displacementAngle);
         Instantiate(projectile, gameObject.transform.position, Quaternion.Euler(0, 0, displacementAngle));
     }
+
+    private IEnumerator TestFire()
+    {
+        while(true)
+        {
+            if (fireProjectiles)
+            {
+                FireProjectile();
+                yield return new WaitForSeconds(shotDelay);
+            } else
+            {
+                yield return new WaitForSeconds(shotDelay);
+            }
+        }
+    }
+
 
 }
